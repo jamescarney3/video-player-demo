28051f26c96b5c3a00954a000b79f8c4e08d59c8
James Carney <jmcarneyiii@gmail.com>
2016-08-30 23:21:10 -0400

    Stub out clip constructor, App methods to validate and add

--- app/js/App.jsx
+++ app/js/App.jsx
@@ -2,6 +2,14 @@ import React, { PropTypes } from 'react';
 
 import Player from './Player.jsx';
 
+// stubbed out Clip constructor function
+function Clip(options) {
+  this.id = options.id;
+  this.name = options.name;
+  this.start = options.start;
+  this.end = options.end;
+}
+
 const App = React.createClass({
   propTypes: {
     children: PropTypes.node,
@@ -10,6 +18,17 @@ const App = React.createClass({
     return ({
       clips: [],
     });
+  },
+  validateClip(newClip) {
+    console.log(newClip);
+    return true;
+  },
+  addClip(options) {
+    const newClip = new Clip(options);
+    if (this.validateClip(newClip)) {
+      const newClips = this.state.clips.concat(newClip);
+      this.setState({ clips: newClips });
+    }
   },
   render() {
     return (
